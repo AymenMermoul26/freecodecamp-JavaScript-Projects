439a5004cba7f07f396f54446c6ad341d4bc6617
Aymen <aymenmermoul@gmail.com>
2025-09-23 08:27:51 +0100

    NavBar

---
+++ Navbar/main.js
@@ -0,0 +1,9 @@
+
+let menu = document.getElementById("menu");
+let links = document.querySelector(".links");
+
+
+menu.addEventListener('click', function () {
+    links.classList.toggle("hide");
+})
+
